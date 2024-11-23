a2423a7013f970f8fd3f332513998a97e5822b7a
kumapapa2012 <morimorikenken29@hotmail.co.jp>
2024-11-23 17:31:16 +0900

    Cleanup Fortnite Replay Parser

--- replay_parser/Program.cs
+++ replay_parser/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using FortniteReplayReader;
 
 // See https://aka.ms/new-console-template for more information
 // Thanks to https://github.com/Shiqan/FortniteReplayDecompressor/tree/master
+// To make release buid, run
+// > dotnet publish --configuration Release
+// To deploy(for me)
+// > cp bin\Release\net9.0\win-x64\publish\_Fortnite.exe C:\Users\morim\kmori_bin
 
 static string FormNumber(int num)
 {
@@ -32,9 +37,10 @@ static string FormNumber(int num)
     }
 }
 
-const int TIME_OSSFET = -5;
+const int TIME_OFFSET = -5;
 var replayFile = "C:\\Users\\morim\\Documents\\_Fortnite\\UnsavedReplay-2024.09.07-03.30.12.replay";
 var myEpicId = "4679c513a0614c889148f61ec69aa749";
+var offset=TIME_OFFSET;
 
 // override replayFile and myEpicId
 if(args.Length > 0 ){
@@ -43,6 +49,18 @@ if(args.Length > 0 ){
     if(args.Length > 1 ) {
         myEpicId = args[1];
         Console.WriteLine("myEpicId has been overridden by cmdline");
+        if(args.Length > 2 ) {
+            if (Int32.TryParse(args[2], out int j))
+            {
+                offset = j;
+                Console.WriteLine("offset has been overridden by cmdline");
+            }
+            else
+            {
+                Console.WriteLine("String could not be parsed for offset.");
+                Environment.Exit(1);
+            }            
+        }
     }
 }
 
@@ -53,7 +71,7 @@ var myKills = replay.Eliminations.Where(c => c.Eliminator == myEpicId.ToUpper())
 for (var i = 0; i < myKills.Count() ; i++)
 {
     var killedAt = DateTime.ParseExact(myKills[i].Time, "mm:ss",null);
-    Console.WriteLine( FormNumber(i + 1) + " : " + killedAt.AddSeconds(TIME_OSSFET).ToString("mm:ss") );  
+    Console.WriteLine( FormNumber(i + 1) + " : " + killedAt.AddSeconds(offset).ToString("mm:ss") );  
 } 
-Console.WriteLine(args);
-Console.WriteLine(args.Length);
+// Console.WriteLine(args);
+// Console.WriteLine(args.Length);
